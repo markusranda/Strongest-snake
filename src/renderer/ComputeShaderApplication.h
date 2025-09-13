cb4aed238add290747a54226ccd54f489002f639
markusranda <randamarkus.95@gmail.com>
2025-09-13 15:13:42 +0200

    main is now simple

---
+++ src/renderer/ComputeShaderApplication.h
@@ -0,0 +1,193 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "renderer/Window.h"
+#include "renderer/SwapChain.h"
+#include "Logger.h"
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
+#include <array>
+#include <optional>
+#include <set>
+#include <random>
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+const uint32_t PARTICLE_COUNT = 8192;
+
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+const std::vector<const char *> deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+
+struct UniformBufferObject
+{
+    float deltaTime = 1.0f;
+};
+
+struct Particle
+{
+    glm::vec2 position;
+    glm::vec2 velocity;
+    glm::vec4 color;
+
+    static VkVertexInputBindingDescription getBindingDescription()
+    {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Particle);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
+    {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Particle, position);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Particle, color);
+
+        return attributeDescriptions;
+    }
+};
+
+struct QueueFamilyIndices
+{
+    std::optional<uint32_t> graphicsAndComputeFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete()
+    {
+        return graphicsAndComputeFamily.has_value() && presentFamily.has_value();
+    }
+};
+
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
+class ComputeShaderApplication
+{
+public:
+    ComputeShaderApplication(uint32_t width, uint32_t height);
+
+    void run();
+
+private:
+    uint32_t width;
+    uint32_t height;
+    Window window;
+
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+    VkSurfaceKHR surface;
+
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+
+    VkQueue graphicsQueue;
+    VkQueue computeQueue;
+    VkQueue presentQueue;
+
+    QueueFamilyIndices queueFamilies;
+
+    SwapChain swapchain;
+
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
+
+    VkDescriptorSetLayout computeDescriptorSetLayout;
+    VkPipelineLayout computePipelineLayout;
+    VkPipeline computePipeline;
+
+    VkCommandPool commandPool;
+
+    std::vector<VkBuffer> shaderStorageBuffers;
+    std::vector<VkDeviceMemory> shaderStorageBuffersMemory;
+
+    std::vector<VkBuffer> uniformBuffers;
+    std::vector<VkDeviceMemory> uniformBuffersMemory;
+    std::vector<void *> uniformBuffersMapped;
+
+    VkDescriptorPool descriptorPool;
+    std::vector<VkDescriptorSet> computeDescriptorSets;
+
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::vector<VkCommandBuffer> computeCommandBuffers;
+
+    std::vector<VkSemaphore> imageAvailableSemaphores;
+    std::vector<VkSemaphore> renderFinishedSemaphores;
+    std::vector<VkSemaphore> computeFinishedSemaphores;
+    std::vector<VkFence> inFlightFences;
+    std::vector<VkFence> computeInFlightFences;
+    uint32_t currentFrame = 0;
+
+    float lastFrameTime = 0.0f;
+
+    double lastTime = 0.0f;
+
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+    void createInstance();
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+    void setupDebugMessenger();
+    void createSurface();
+    void pickPhysicalDevice();
+    void createSwapChain();
+    void createLogicalDevice();
+    void createRenderPass();
+    void createComputeDescriptorSetLayout();
+    void createGraphicsPipeline();
+    void createComputePipeline();
+    void createFramebuffers();
+    void createCommandPool();
+    void createShaderStorageBuffers();
+    void createUniformBuffers();
+    void createDescriptorPool();
+    void createComputeDescriptorSets();
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer &buffer, VkDeviceMemory &bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createCommandBuffers();
+    void createComputeCommandBuffers();
+    void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+    void recordComputeCommandBuffer(VkCommandBuffer commandBuffer);
+    void createSyncObjects();
+    void updateUniformBuffer(uint32_t currentImage);
+    void drawFrame();
+    VkShaderModule createShaderModule(const std::vector<char> &code);
+    bool isDeviceSuitable(VkPhysicalDevice device, VkSurfaceKHR surface);
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    std::vector<const char *> getRequiredExtensions();
+
+    bool checkValidationLayerSupport();
+
+    static std::vector<char> readFile(const std::string &filename);
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData);
+};
