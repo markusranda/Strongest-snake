4bf1d3c526b1c4d246d97aed6d9690067ef10f10
markusranda <randamarkus.95@gmail.com>
2025-09-13 12:37:27 +0200

    fixed MSAA warning

--- src/main.cpp
+++ src/main.cpp
@@ -687,7 +687,7 @@ private:
         multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
         multisampling.sampleShadingEnable = VK_FALSE;
 
-        multisampling.rasterizationSamples = VK_SAMPLE_COUNT_8_BIT;
+        multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
 
         VkPipelineColorBlendAttachmentState colorBlendAttachment{};
         colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
