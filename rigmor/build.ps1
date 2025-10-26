30bcec41b122cffdc57e574ceda8e6580fce65cc
markusranda <randamarkus.95@gmail.com>
2025-10-26 11:19:33 +0100

    Almost forgot to include build script

---
+++ rigmor/build.ps1
@@ -0,0 +1,5 @@
+cls; 
+$envSetup = '"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64'; 
+$buildCmd = 'cl /TP /std:c++17 /EHsc /Zi /Od /DDEBUG rigmor\rigmor.hpp /D RIGMOR_IMPLEMENTATION /Fo:build\rigmor\ /Fe:build\rigmor\rigmor.exe /Fd:build\rigmor\rigmor.pdb'; 
+New-Item -ItemType Directory -Force -Path build\rigmor | Out-Null; 
+cmd /c "$envSetup && $buildCmd"
